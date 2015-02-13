d5b26532c5812ca193c06c0623957e9636861905
cmdrdahkron <cmdrdahkron@gmail.com>
2015-02-13 16:59:50 +0000

    Toying

--- native/windows/companion/Native.cs
+++ native/windows/companion/Native.cs
@@ -7,17 +7,18 @@ using System.Threading.Tasks;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using System.Threading;
-using IronRuby;
-using IronRuby.Builtins;
-using Microsoft.Scripting.Hosting;
-using System.IO;
+
+//using IronRuby;
+//using IronRuby.Builtins;
+//using Microsoft.Scripting.Hosting;
+//using System.IO;
 
 namespace companion {
   class Native {
     public SpeechRecognitionEngine speechEngine = null;
-    public ScriptEngine            scriptEngine = null;
+    //public ScriptEngine            scriptEngine = null;
 
-    public FileSystemWatcher fileSystemWatcher = null;
+    //public FileSystemWatcher fileSystemWatcher = null;
 
     public Dictionary<string, object> vars = null;
 
@@ -29,12 +30,12 @@ namespace companion {
       System.Console.WriteLine("Reloading...");
 
       speechEngine.UnloadAllGrammars();
-      scriptEngine.ExecuteFile("main.rb");
+      //scriptEngine.ExecuteFile("main.rb");
     }
 
-    public void On(String pattern, RubyArray values) {
-      System.Console.WriteLine("Registering pattern: " + pattern);
-    }
+    //public void On(String pattern, RubyArray values) {
+    //  System.Console.WriteLine("Registering pattern: " + pattern);
+    //}
 
     public void Play(String filename) {
 
@@ -53,60 +54,64 @@ namespace companion {
     }
 
     private void CreateScriptEngine() {
-      vars = new Dictionary<string, object>();
 
-      scriptEngine = Ruby.CreateEngine();
-
-#if DEBUG
-      string baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..");
-#else
-      string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-#endif
-
-      string rubylibDir = Path.Combine(baseDir, "rubylib");
-      string scriptsDir = Path.Combine(baseDir, "scripts");
-
-      List<string> searchPaths = new List<string>();
-      // searchPaths.Add(baseDir);
-      searchPaths.Add(Path.Combine(rubylibDir, "IronRuby"));
-      searchPaths.Add(Path.Combine(rubylibDir, "ruby", "site_ruby", "1.9.1"));
-      searchPaths.Add(Path.Combine(rubylibDir, "ruby", "site_ruby"));
-      searchPaths.Add(Path.Combine(rubylibDir, "ruby", "1.9.1"));
-      searchPaths.Add(Path.Combine(rubylibDir, "vendor"));
-      scriptEngine.SetSearchPaths(searchPaths);
-
-      scriptEngine.Runtime.Globals.SetVariable("Native", this);
-
-      //fileSystemWatcher = new FileSystemWatcher(baseDir, rubylibDir);
-      //fileSystemWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-      //fileSystemWatcher.IncludeSubdirectories = true;
-      //fileSystemWatcher.Filter = "*.rb";
-      //fileSystemWatcher.Changed += fileSystemWatcher_Changed;
-      //fileSystemWatcher.Created += fileSystemWatcher_Created;
-      //fileSystemWatcher.Deleted += fileSystemWatcher_Deleted;
-      //fileSystemWatcher.Renamed += fileSystemWatcher_Renamed;
-      //fileSystemWatcher.EnableRaisingEvents = true;
     }
 
-    void fileSystemWatcher_Renamed(object sender, RenamedEventArgs e) {
-      Reload();
-    }
+//    private void CreateScriptEngine() {
+//      vars = new Dictionary<string, object>();
 
-    void fileSystemWatcher_Deleted(object sender, FileSystemEventArgs e) {
-      Reload();
-    }
+//      scriptEngine = Ruby.CreateEngine();
 
-    void fileSystemWatcher_Created(object sender, FileSystemEventArgs e) {
-      Reload();
-    }
+//#if DEBUG
+//      string baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..");
+//#else
+//      string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+//#endif
 
-    void fileSystemWatcher_Changed(object sender, FileSystemEventArgs e) {
-      Reload();
-    }
+//      string rubylibDir = Path.Combine(baseDir, "rubylib");
+//      string scriptsDir = Path.Combine(baseDir, "scripts");
+
+//      List<string> searchPaths = new List<string>();
+//      // searchPaths.Add(baseDir);
+//      searchPaths.Add(Path.Combine(rubylibDir, "IronRuby"));
+//      searchPaths.Add(Path.Combine(rubylibDir, "ruby", "site_ruby", "1.9.1"));
+//      searchPaths.Add(Path.Combine(rubylibDir, "ruby", "site_ruby"));
+//      searchPaths.Add(Path.Combine(rubylibDir, "ruby", "1.9.1"));
+//      searchPaths.Add(Path.Combine(rubylibDir, "vendor"));
+//      scriptEngine.SetSearchPaths(searchPaths);
+
+//      scriptEngine.Runtime.Globals.SetVariable("Native", this);
+
+//      //fileSystemWatcher = new FileSystemWatcher(baseDir, rubylibDir);
+//      //fileSystemWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+//      //fileSystemWatcher.IncludeSubdirectories = true;
+//      //fileSystemWatcher.Filter = "*.rb";
+//      //fileSystemWatcher.Changed += fileSystemWatcher_Changed;
+//      //fileSystemWatcher.Created += fileSystemWatcher_Created;
+//      //fileSystemWatcher.Deleted += fileSystemWatcher_Deleted;
+//      //fileSystemWatcher.Renamed += fileSystemWatcher_Renamed;
+//      //fileSystemWatcher.EnableRaisingEvents = true;
+//    }
+
+    //void fileSystemWatcher_Renamed(object sender, RenamedEventArgs e) {
+    //  Reload();
+    //}
+
+    //void fileSystemWatcher_Deleted(object sender, FileSystemEventArgs e) {
+    //  Reload();
+    //}
+
+    //void fileSystemWatcher_Created(object sender, FileSystemEventArgs e) {
+    //  Reload();
+    //}
+
+    //void fileSystemWatcher_Changed(object sender, FileSystemEventArgs e) {
+    //  Reload();
+    //}
 
     public void Start() {
       CreateSpeechEngine();
-      CreateScriptEngine();
+      //CreateScriptEngine();
 
       Reload();
     }
